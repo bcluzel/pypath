0bfc2645873085c714eaffed6e297a8c5512e838
bcluzel <baptiste.clu@gmail.com>
2020-03-24 13:20:56 +0000

    Bump version: 0.2.3 → 0.2.4 [skip ci]

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ castar = Extension(module_name +".castar",
 
 setup(
     name=module_name + "-fpa", # change name for pypi not module
-    version='0.2.30',
+    version='0.2.40',
     description='Fast Pathfinding Algorithm',
     long_description=long_description,
     long_description_content_type='text/markdown',
